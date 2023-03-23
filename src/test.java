b7bbb89546ebd1b2beba3921f2da2affc2b2fb43
Jacob Kocan <jacob.jk.kocan@gmail.com>
2023-03-23 19:07:09 -0400

    Version 2

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         test myobj = new test();
         System.out.println(myobj.x);
+        System.out.println("New version");
     }
 }
