9add0669321b746543b06b347d8e80937624b6fe
Jacob Kocan <jacob.jk.kocan@gmail.com>
2023-03-23 19:19:42 -0400

    Version 1 restored:

--- src/test.java
+++ src/test.java
@@ -4,6 +4,5 @@ public class test {
     public static void main(String[] args) {
         test myobj = new test();
         System.out.println(myobj.x);
-        System.out.println("New version 3");
     }
 }
